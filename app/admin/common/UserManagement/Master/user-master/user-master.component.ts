c45785469c9a6c25e4fe2898ef38d1b60e3ba98c
Shrabani Debnath <shrabani9797.sd@gmail.com>
2022-04-19 15:31:26 +0530

    update(user_master)

---
+++ app/admin/common/UserManagement/Master/user-master/user-master.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-master',
+  templateUrl: './user-master.component.html',
+  styleUrls: ['./user-master.component.css']
+})
+export class UserMasterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
