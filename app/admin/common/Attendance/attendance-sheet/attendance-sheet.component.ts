6d6515c2225aa57a5d3d124742b4e5f764ce37d4
Compacct <compacct.cloud@gmail.com>
2022-11-17 14:44:30 +0530

    Update attendance-sheet.component.ts

--- app/admin/common/Attendance/attendance-sheet/attendance-sheet.component.ts
+++ app/admin/common/Attendance/attendance-sheet/attendance-sheet.component.ts
@@ -92,7 +92,6 @@ export class AttendanceSheetComponent implements OnInit {
   generate = false;
   download = false;
   AllattendancestatusFormSubmitted = false;
-  attndata:any = [];
   constructor(
     private route : ActivatedRoute,
     private Header: CompacctHeader,
@@ -449,8 +448,6 @@ export class AttendanceSheetComponent implements OnInit {
 
     }
     this.GlobalAPI.getData(obj).subscribe((data:any)=>{
-      this.attndata = data;
-      console.log("excel attndata ===",this.attndata)
       if (data[0].Success === "False") {
         this.compacctToast.clear();
         this.compacctToast.add({
@@ -463,7 +460,6 @@ export class AttendanceSheetComponent implements OnInit {
       else {
         const worksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(data);
         const workbook: XLSX.WorkBook = {Sheets: {'data': worksheet}, SheetNames: ['data']};
-        // console.log("excel ===",workbook)
         XLSX.writeFile(workbook, fileName+'.xlsx');
       }
     })
