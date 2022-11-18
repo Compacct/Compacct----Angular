1caeb9ec6ee82f99c3e429ca85fc3f6076d296f4
Compacct <compacct.cloud@gmail.com>
2022-11-18 13:30:53 +0530

    18 nov ----s2

---
+++ app/admin/common/MICL/return-material/return-material.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-return-material',
+  templateUrl: './return-material.component.html',
+  styleUrls: ['./return-material.component.css']
+})
+export class ReturnMaterialComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
