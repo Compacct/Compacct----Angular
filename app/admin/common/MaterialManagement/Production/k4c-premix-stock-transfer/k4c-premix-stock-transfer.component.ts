6cff58894b9cc1db0f3680b479304cff31e79c76
Compacct <compacct.cloud@gmail.com>
2022-11-24 11:49:28 +0530

    24 nov ----s1

---
+++ app/admin/common/MaterialManagement/Production/k4c-premix-stock-transfer/k4c-premix-stock-transfer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-k4c-premix-stock-transfer',
+  templateUrl: './k4c-premix-stock-transfer.component.html',
+  styleUrls: ['./k4c-premix-stock-transfer.component.css']
+})
+export class K4cPremixStockTransferComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
