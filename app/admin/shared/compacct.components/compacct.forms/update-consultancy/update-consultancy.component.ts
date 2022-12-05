8f0348276cf2d4633bc8e882e55e470e8af7d19f
Debabrata909 <72221706+Debabrata909@users.noreply.github.com>
2022-12-05 19:08:50 +0530

    update

--- app/admin/shared/compacct.components/compacct.forms/update-consultancy/update-consultancy.component.ts
+++ app/admin/shared/compacct.components/compacct.forms/update-consultancy/update-consultancy.component.ts
@@ -37,7 +37,7 @@ export class UpdateConsultancyComponent implements OnInit {
       console.log("this.Objconsultancy.Level_1_Status",this.Objconsultancy.Level_1_Status)
     })
   }
-  getSecondL(){
+  getSecondL(edit?:any){
     this.Objconsultancy.Level_2_Status = ''
     this.Objconsultancy.Level_3_Status = ''
     this.SecondL = this.FirstLevelList.length ? this.FirstLevelList.filter((el:any) => Number(el.Level_1_ID) == Number(this.Objconsultancy.Level_1_Status))[0].Level_1_Status : ''
@@ -52,14 +52,17 @@ export class UpdateConsultancyComponent implements OnInit {
        }
       this.GlobalAPI.postData(obj).subscribe((data:any)=>{
         this.SecondLevelList = data
-        this.Objconsultancy.Level_2_Status = this.SecondLevelList.length ? data[0].Level_2_ID.toString() : ""
-        this.getThirdL()
-        console.log("SecondLevelList",data)
+        if(!edit){
+          this.Objconsultancy.Level_2_Status = this.SecondLevelList.length ? data[0].Level_2_ID.toString() : ""
+           this.getThirdL()
+        }
+     
+        
       })
     }
     
   }
-  getThirdL(){
+  getThirdL(edit?:any){
     this.Objconsultancy.Level_3_Status = ''
     this.ThirdL = this.SecondLevelList.length ? this.SecondLevelList.filter((el:any) => Number(el.Level_2_ID) == Number(this.Objconsultancy.Level_2_Status))[0].Level_2_Status : ''
     this.eventEmit()
@@ -72,8 +75,10 @@ export class UpdateConsultancyComponent implements OnInit {
        }
       this.GlobalAPI.postData(obj).subscribe((data:any)=>{
         this.ThirdLevelList = data
-        this.Objconsultancy.Level_3_Status = this.ThirdLevelList.length ? data[0].Level_3_ID.toString() : ""
-        console.log("ThirdLevelList",data)
+         console.log("ThirdLevelList",data)
+        if(!edit){
+          this.Objconsultancy.Level_3_Status = this.ThirdLevelList.length ? data[0].Level_3_ID.toString() : ""
+        }
         this.eventEmit()
       })
     }
@@ -137,6 +142,32 @@ export class UpdateConsultancyComponent implements OnInit {
     this.SecondL = ""
     this.ThirdL = ""
   }
+  editConsulyancy(obj:editObj){
+    console.log("obj",obj)
+    
+     const FilterFirstLevelList = this.FirstLevelList.filter((el:any)=> el.Level_1_Status == obj.Level_1_Status )
+     if(FilterFirstLevelList.length){
+      this.Objconsultancy.Level_1_Status = FilterFirstLevelList[0].Level_1_ID
+      this.getSecondL(true)
+      setTimeout(() => {
+        const FilterSecondLevelList = this.SecondLevelList.filter((el:any)=> el.Level_2_Status == obj.Level_2_Status )
+        if(FilterSecondLevelList.length){
+          this.Objconsultancy.Level_2_Status = FilterSecondLevelList[0].Level_2_ID.toString()
+          this.getThirdL(true)
+         } 
+        setTimeout(() => {
+          const FilterThirdLevelList = this.ThirdLevelList.filter((el:any)=> el.Level_3_Status == obj.Level_3_Status )
+          if(FilterThirdLevelList.length){
+          this.Objconsultancy.Level_3_Status = FilterThirdLevelList[0].Level_3_ID.toString()
+          this.ChangeThirdL()
+         }
+        }, 500);
+      }, 500);
+    
+       } 
+    
+   
+  }
 }
 class consultancy{
   Level_1_Status:any = "";
@@ -145,4 +176,9 @@ class consultancy{
 }
 interface inputValue{
   required:boolean
+}
+interface editObj{
+  Level_1_Status:string
+  Level_2_Status:string
+  Level_3_Status: string
 }
