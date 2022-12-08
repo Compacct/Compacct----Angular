b79dabf8484cb6adcc27ab35fe17bab8fa24683d
Ranjit7333 <ranjitkumardas242@gmail.com>
2022-12-08 12:48:48 +0530

    update 08-12-2022

---
+++ app/admin/common/CRM/BSHPL-FOLLOWUP/bshpl-hf-follow-up/bshpl-hf-follow-up.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bshpl-hf-follow-up',
+  templateUrl: './bshpl-hf-follow-up.component.html',
+  styleUrls: ['./bshpl-hf-follow-up.component.css']
+})
+export class BSHPLHfFollowUpComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
