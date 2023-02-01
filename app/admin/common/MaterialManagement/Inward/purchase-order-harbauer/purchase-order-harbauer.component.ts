d487b16eb7918434eee22c474e173906fd4b0cb6
Debabrata909 <72221706+Debabrata909@users.noreply.github.com>
2023-02-01 17:07:25 +0530

    update

---
+++ app/admin/common/MaterialManagement/Inward/purchase-order-harbauer/purchase-order-harbauer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-purchase-order-harbauer',
+  templateUrl: './purchase-order-harbauer.component.html',
+  styleUrls: ['./purchase-order-harbauer.component.css']
+})
+export class PurchaseOrderHarbauerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
