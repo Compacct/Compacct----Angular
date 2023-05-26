84ba67b036de7e0e20cdc4311b9621126436f1ec
Compacct <90822923+Compacct@users.noreply.github.com>
2023-05-26 12:02:31 +0530

    updated 26th May 2023

--- app/admin/layout.module.ts
+++ app/admin/layout.module.ts
@@ -472,6 +472,7 @@ import { MasterProjectTeamComponent } from './common/Harbauer/master-project-tea
 import { HrCouponReportComponent } from './common/MICL/hr-coupon-report/hr-coupon-report.component';
 import { JohDailyAttendanceComponent } from './common/Attendance/joh-daily-attendance/joh-daily-attendance.component';
 import { CandidateInterviewProcessComponent } from './common/Attendance/candidate-interview-process/candidate-interview-process.component';
+import { ConsultancyPaymentComponent } from './common/Attendance/consultancy-payment/consultancy-payment.component';
 
 @NgModule({
   declarations: [
@@ -860,7 +861,8 @@ import { CandidateInterviewProcessComponent } from './common/Attendance/candidat
     MasterProjectTeamComponent,
     HrCouponReportComponent,
     JohDailyAttendanceComponent,
-    CandidateInterviewProcessComponent
+    CandidateInterviewProcessComponent,
+    ConsultancyPaymentComponent
 
 
   ],
