4b3527ae87ec415c29071db7e3a37435817ef98c
ranjit <103984085+Ranjit7333@users.noreply.github.com>
2023-06-01 11:34:12 +0530

    Update New Comp 01-06-2023

---
+++ app/admin/common/Harbauer/bl-txn-product-return-from-project/bl-txn-product-return-from-project.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bl-txn-product-return-from-project',
+  templateUrl: './bl-txn-product-return-from-project.component.html',
+  styleUrls: ['./bl-txn-product-return-from-project.component.css']
+})
+export class BLTxnProductReturnFromProjectComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
