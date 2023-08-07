efce8e793d22b0aee559f4da16ca0193e0b4327e
Compacct <90822923+Compacct@users.noreply.github.com>
2023-08-07 11:18:21 +0530

    07.08.2023 11:18 AM Update

--- app/admin/common/HR/training-grade-module/training-grade-module.component.ts
+++ app/admin/common/HR/training-grade-module/training-grade-module.component.ts
@@ -1,15 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { MessageService } from 'primeng/api';
+import { DateTimeConvertService } from '../../../shared/compacct.global/dateTime.service';
+import { CompacctCommonApi } from '../../../shared/compacct.services/common.api.service';
+import { CompacctHeader } from '../../../shared/compacct.services/common.header.service';
+import { CompacctGlobalApiService } from '../../../shared/compacct.services/compacct.global.api.service';
 
 @Component({
   selector: 'app-training-grade-module',
   templateUrl: './training-grade-module.component.html',
-  styleUrls: ['./training-grade-module.component.css']
+  styleUrls: ['./training-grade-module.component.css'],
+  providers: [MessageService],
+  encapsulation: ViewEncapsulation.None
 })
 export class TrainingGradeModuleComponent implements OnInit {
-
-  constructor() { }
+  tabIndexToView: number = 0;
+  Items: any = ['BROWSE', 'CREATE'];
+  Spinner: boolean = false;
+  buttonname: string = 'Save';
+  traningNoList:[] = [];
+  SerachFormSubmitted:boolean = false;
+  objSearchData = new searchData();
+  constructor(
+    private Header: CompacctHeader,
+    private CompacctToast: MessageService,
+    private GlobalAPI: CompacctGlobalApiService,
+    private commonApi: CompacctCommonApi,
+    private DateService: DateTimeConvertService
+  ) { }
 
   ngOnInit() {
+    this.Header.pushHeader({
+      Header: "Training Grade",
+      Link: "JOH HR --> Training Grade"
+    });
   }
 
+  TabClick(e: any) {
+    this.tabIndexToView = e.index;
+    this.clearData();
+  }
+
+  onConfirm() {
+  }
+
+  onReject() {
+    this.CompacctToast.clear("c");
+  }
+
+  clearData() {
+
+  }
+}
+
+class searchData {
+  Training_No:any;
 }
