e6c7b898dd69a2fa0e53875fa76c86634bc06517
Compacct <compacct.cloud@gmail.com>
2023-11-03 15:37:42 +0530

    3 nov

--- app/admin/common/MaterialManagement/Outward/micl-requisition/micl-requisition.component.ts
+++ app/admin/common/MaterialManagement/Outward/micl-requisition/micl-requisition.component.ts
@@ -126,6 +126,7 @@ export class MiclRequisitionComponent implements OnInit {
   Department_ID:any;
   SelectedDistcreatedbyDepartment:any = [];
   DistcreatedbyDepartment:any = [];
+  saveSpinner:boolean = false;
 
   constructor(private $http: HttpClient,
     private commonApi: CompacctCommonApi,
@@ -417,6 +418,7 @@ export class MiclRequisitionComponent implements OnInit {
   }
   SaveRequi(valid){ 
    //console.log("valid",valid);
+   this.saveSpinner = false;
    this.reqiFormSubmitted = true;
    this.validatation.required = true;
    this.ngxService.start();
@@ -531,6 +533,7 @@ export class MiclRequisitionComponent implements OnInit {
   onConfirmSave(){
       let saveData:any = [];
       let paramlist:any = [];
+      this.saveSpinner = true;
       // let mgs = "";
       // if(this.ReqNo){
  
@@ -609,6 +612,7 @@ export class MiclRequisitionComponent implements OnInit {
            });
           
            // this.SaveNPrintBill();
+           this.saveSpinner = false;
            this.clearData();
             this.Department_ID = undefined;
             this.Requisition_ID = undefined;
@@ -621,6 +625,7 @@ export class MiclRequisitionComponent implements OnInit {
             this.items = ["BROWSE", "CREATE", "STOCK", "STATUS", "MIS"];
             this.buttonname = "Save";
             } else{
+              this.saveSpinner = false;
               this.Spinner = false;
               this.ngxService.stop();
               this.compacctToast.add({
@@ -1003,6 +1008,7 @@ export class MiclRequisitionComponent implements OnInit {
   }
   Active(col){
     //console.log("col",col);
+    this.saveSpinner = false;
     this.can_popup = false;
     this.Del = false;
     this.Save = false;
