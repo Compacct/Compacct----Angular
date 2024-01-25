71d57d38beed8dfb1459f96f6f417e919dbaa159
Sudipta <90822923+Compacct@users.noreply.github.com>
2024-01-25 18:36:59 +0530

    updated 25-01-24 7:00PM

--- app/admin/common/FinancialManagement/Transaction/Purchase/purchase-bill-mrp-update/purchase-bill-mrp-update.component.ts
+++ app/admin/common/FinancialManagement/Transaction/Purchase/purchase-bill-mrp-update/purchase-bill-mrp-update.component.ts
@@ -1,15 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { MessageService } from 'primeng/api';
+import { CompacctHeader } from '../../../../../shared/compacct.services/common.header.service';
+import { CompacctGlobalApiService } from '../../../../../shared/compacct.services/compacct.global.api.service';
 
 @Component({
   selector: 'app-purchase-bill-mrp-update',
   templateUrl: './purchase-bill-mrp-update.component.html',
-  styleUrls: ['./purchase-bill-mrp-update.component.css']
+  styleUrls: ['./purchase-bill-mrp-update.component.css'],
+  providers: [MessageService],
+  encapsulation: ViewEncapsulation.None
 })
 export class PurchaseBillMrpUpdateComponent implements OnInit {
-
-  constructor() { }
+  tabIndexToView: number = 0;
+  seachSpinner:boolean=false;
+  CostCenterList:any=[];
+  Tablelist:any=[];
+  TablelistHeader:any=[];
+  objMrpUpdateSearch: MrpUpdateSearch = new MrpUpdateSearch();
+  constructor(
+    private Header: CompacctHeader,
+    private CompacctToast: MessageService,
+    private GlobalAPI: CompacctGlobalApiService,
+  ) { }
 
   ngOnInit() {
+    this.Header.pushHeader({
+      Header: "Purchase Bill MRP Update",
+      Link: "Financial Management --> Transaction --> Purchase --> Purchase Bill MRP Update"
+    });
   }
 
+  getDateRange(dateRangeObj: any) {
+    if (dateRangeObj.length) {
+      this.objMrpUpdateSearch.From_Date = dateRangeObj[0];
+      this.objMrpUpdateSearch.To_Date = dateRangeObj[1];
+    }
+  }
+
+  GetAllBrowseData(){
+
+  }
+
+}
+
+class MrpUpdateSearch{
+  From_Date : any;
+  To_Date : any;
+  CostcenterID:any;
 }
