8d98b2b7364924ca3000e8897f4b408631edb300
Compacct <compacct.cloud@gmail.com>
2024-08-05 14:33:20 +0530

    k4c 5 aug 24

--- app/admin/common/MaterialManagement/Outward/Distribution Challan/k4-c-dispatch-to-outlet/k4-c-dispatch-to-outlet.component.ts
+++ app/admin/common/MaterialManagement/Outward/Distribution Challan/k4-c-dispatch-to-outlet/k4-c-dispatch-to-outlet.component.ts
@@ -379,7 +379,7 @@ export class K4CDispatchToOutletComponent implements OnInit {
     this.GlobalAPI.getData(obj).subscribe((data:any)=>{
       this.toGodownList = data;
       console.log("this.toGodownList",this.toGodownList);
-      this.Objdispatch.To_Godown_ID= this.toGodownList[0].godown_id ;
+      this.Objdispatch.To_Godown_ID= this.toGodownList.length ? this.toGodownList[0].godown_id : undefined;
       if(this.Objdispatch.To_Godown_ID){
         this.To_Godown_ID_Dis = true;
       }
